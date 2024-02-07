b7340b595fe7330f609a95084498b4fc911df895
Andreia Matos <58214313+andreianmatos@users.noreply.github.com>
2024-02-07 14:50:43 +0000

    change to graph model

--- docs/script.js
+++ docs/script.js
@@ -34,8 +34,8 @@ class CVAE {
 async function loadModels() {
     // Load the encoder and decoder models
 
-    const decoder = await tf.loadLayersModel('model_json/decoder_json/model.json');
-    const encoder = await tf.loadLayersModel('model_json/encoder_json/model.json');
+    const decoder = await tf.loadGraphModel('model_json/decoder_json/model.json');
+    const encoder = await tf.loadGraphModel('model_json/encoder_json/model.json');
     
     const latent_dim = 2;
     const loaded_model = new CVAE(latent_dim, encoder, decoder);
